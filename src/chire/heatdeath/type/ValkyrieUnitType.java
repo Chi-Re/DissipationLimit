356ddbe5e6e91365916385af13e25f3d51b98a63
炽热S <3531962676@qq.com>
2026-02-22 23:22:46 +0800

    简单创建自定义单位

---
+++ src/chire/heatdeath/type/ValkyrieUnitType.java
@@ -0,0 +1,37 @@
+package chire.heatdeath.type;
+
+import arc.scene.ui.layout.Table;
+import mindustry.gen.Building;
+import mindustry.gen.Unit;
+import mindustry.type.UnitType;
+
+public class ValkyrieUnitType extends UnitType {
+    public ValkyrieUnitType(String name) {
+        super(name);
+    }
+
+    @Override
+    public void update(Unit unit) {
+        super.update(unit);
+    }
+
+    @Override
+    public void draw(Unit unit) {
+        super.draw(unit);
+    }
+
+    @Override
+    public void display(Unit unit, Table table) {
+        super.display(unit, table);
+    }
+
+    @Override
+    public void drawBody(Unit unit) {
+        super.drawBody(unit);
+    }
+
+    @Override
+    public void drawCell(Unit unit) {
+        super.drawCell(unit);
+    }
+}
